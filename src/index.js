9a3333ec1dfef6c56863311c609e67aa41d469f2
Ntare Guy <gntare2@gmail.com>
2022-03-24 12:33:31 +0200

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import TodoContainer from "./components/TodoContainer"
+import About from './pages/about';
+import NotFound from './pages/NotFound';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <Routes>
+        <Route path='/' element={<TodoContainer />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>  
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
